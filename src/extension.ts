f09fbe4f2bb1b920648f51bb72f9b3a41feb61d8
Saif M Hakeam <saif.hakeam97@gmail.com>
2024-01-16 00:15:53 +0400

    call sub-commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  //Get the configuration for your extension
+  const config = vscode.workspace.getConfiguration("keybing-command-palette-interop");
+  console.log(config);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "keybing-command-palette-interop" is now active!');
+  // Get the commands from the configuration
+  const commands = config.get("commands") as Array<{ id: string; message: string }>;
+  console.log(commands);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('keybing-command-palette-interop.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from keybind &lt;&gt; command palette!');
-	});
+  // Register each command
+  for (const command of commands) {
+    let disposable = vscode.commands.registerCommand(command.id, () => {
+      vscode.window.showInformationMessage(command.message);
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
+  }
+
+  // Register a static command that prompts the user to select a command
+  let disposable = vscode.commands.registerCommand("keybing-command-palette-interop.a", async () => {
+    const commandId = await vscode.window.showQuickPick(
+      commands.map((c) => c.id),
+      { placeHolder: "Select a command" }
+    );
+    if (commandId) {
+      vscode.commands.executeCommand(commandId);
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
